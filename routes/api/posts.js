7bfbe407a41ac37d19d803db548bac7d5c80a435
Melissa Garza <melissa.garza@utexas.edu>
2020-03-22 20:32:07 -0500

    Add and remove comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -126,4 +126,62 @@ router.put('/unlike/:post_id', auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/posts/comment/:post_id
+// @desc     Comment on a post
+// @access   Private
+router.put('/comment/:post_id', [auth, [
+  check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.post_id);
+    if (!post) return res.status(404).json({ msg: 'Post does not exist' });
+
+    const newComment = {
+      text: req.body.text,
+      user: user.id,
+      name: user.name,
+      avatar: user.avatar
+    };
+    post.comments.unshift(newComment);
+  
+    await post.save();
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') return res.status(404).json({ msg: 'Post does no exist' });
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    DELETE api/posts/comment/:post_id/:comment_id
+// @desc     Delete a comment based on post ID and comment ID
+// @access   Private
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    if (!post) return res.status(404).json({ msg: 'Post does not exist' });
+
+    const indexOfComment = post.comments.findIndex(comment => comment.id === req.params.comment_id);
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    if (!comment) return res.status(404).json({ msg: 'Comment does not exist' });
+    if (comment.user.toString() !== req.user.id) return res.status(401).json({ msg: 'User not authorized' });
+
+    post.comments.splice(indexOfComment, 1);
+  
+    await post.save();
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') return res.status(404).json({ msg: 'Post or comment does not exist' });
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
