57afe229ac50e96d7f79daf617cad90d45e0d980
Melissa Garza <melissa.garza@utexas.edu>
2020-04-01 17:58:47 -0500

    Load user and set auth token

--- client/src/store.js
+++ client/src/store.js
@@ -11,4 +11,22 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
+// set up a store subscription listener
+// to store the user's token in localStorage
+let currentState;
+store.subscribe(() => {
+  // keep track of the previous and current state to compare changes
+  let previousState = currentState;
+  currentState = store.getState();
+  // if the token changes, set the value in localStorage
+  if (previousState && previousState.auth.token !== currentState.auth.token) {
+    const token = currentState.auth.token;
+    if (token) {
+      localStorage.setItem('token', token);
+    } else {
+      localStorage.removeItem('token');
+    }
+  }
+});
+
 export default store;
