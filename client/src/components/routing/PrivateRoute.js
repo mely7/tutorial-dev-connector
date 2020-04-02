83a4268f6ef106e9c101ad75b0555a35a16b5a22
Melissa Garza <melissa.garza@utexas.edu>
2020-04-02 15:21:35 -0500

    Protected route for dashboard

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ auth: { isAuthenticated, loading }, component: Component, ...rest }) => {
+  return (
+    <Route {...rest} render={props => !isAuthenticated && !loading ? (<Redirect to='/login' />) : (<Component {...props} />)} />
+  )
+};
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
