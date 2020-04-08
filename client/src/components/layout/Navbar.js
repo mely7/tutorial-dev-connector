7833b7d19ec96b75f9df452a57eba46ae844bd91
Melissa Garza <melissa.garza@utexas.edu>
2020-04-08 11:45:05 -0500

    Display profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,11 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/profiles">
+          Developers
+        </Link>
+      </li>
+      <li>
         <Link to="/dashboard">
           <i className="fas fa-user">{' '}</i>
           <span className="hide-sm">Dashboard</span>
@@ -23,9 +28,21 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
   );
   const guestLinks = (
     <ul>
-      <li><a href="#!">Developers</a></li>
-      <li><Link to="/register">Register</Link></li>
-      <li><Link to="/login">Login</Link></li>
+      <li>
+        <Link to="/profiles">
+          Developers
+        </Link>
+      </li>
+      <li>
+        <Link to="/register">
+          Register
+        </Link>
+      </li>
+      <li>
+        <Link to="/login">
+          Login
+        </Link>
+      </li>
     </ul>
   );
 
