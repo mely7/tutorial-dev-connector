2076be942426c2be19911942c71f78ee68a8344f
Melissa Garza <melissa.garza@utexas.edu>
2020-04-08 17:15:51 -0500

    Displaying GitHub repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGitHub from './ProfileGitHub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -56,6 +57,12 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
               </Fragment>
             ) : (<h4>No education credentials</h4>)}
           </div>
+
+          {profile.githubusername && (
+            <ProfileGitHub
+              username={profile.githubusername}
+            />
+          )}
         </div>
       </Fragment>}
     </Fragment>
