e9fd10da13bd13db482f049cb866e2b25178b9e0
Melissa Garza <melissa.garza@utexas.edu>
2020-04-08 17:03:27 -0500

    Profile experience and education display

---
+++ client/src/components/profile/ProfileEducation.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const ProfileEducation = ({education: {
+  school,
+  degree,
+  fieldofstudy,
+  from,
+  to,
+  description
+}}) => (
+  <div>
+    <h2 className="text-dark">{school}</h2>
+    <p>
+      <Moment format="YYYY/MM/DD">{from}</Moment> - {!to ? 'Now' : <Moment format="YYYY/MM/DD">{to}</Moment>}
+    </p>
+    <p>
+      <strong>Degree: </strong> {degree}
+    </p>
+    <p>
+      <strong>Field of Study: </strong> {fieldofstudy}
+    </p>
+    <p>
+      <strong>Description: </strong> {description}
+    </p>
+  </div>
+);
+
+ProfileEducation.propTypes = {
+  experience: PropTypes.array.isRequired
+}
+
+export default ProfileEducation;
---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const ProfileExperience = ({experience: {
+  company,
+  title,
+  location,
+  current,
+  from,
+  to,
+  description
+}}) => (
+  <div>
+    <h2 className="text-dark">{company}</h2>
+    <p>
+      <Moment format="YYYY/MM/DD">{from}</Moment> - {!to ? 'Now' : <Moment format="YYYY/MM/DD">{to}</Moment>}
+    </p>
+    <p>
+      <strong>Postion: </strong> {title}
+    </p>
+    <p>
+      <strong>Description: </strong> {description}
+    </p>
+  </div>
+);
+
+ProfileExperience.propTypes = {
+  experience: PropTypes.array.isRequired
+}
+
+export default ProfileExperience;
