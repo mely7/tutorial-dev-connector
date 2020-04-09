f610ea0d593ae6482a1cfd2fd32f0abb1990ca65
Melissa Garza <melissa.garza@utexas.edu>
2020-04-09 17:31:15 -0500

    Like and unlike functionality

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,6 +1,7 @@
 import {
   GET_POSTS,
-  POST_ERROR
+  POST_ERROR,
+  UPDATE_LIKES
 } from '../actions/types';
 
 const initialState = {
@@ -18,6 +19,14 @@ export default (state = initialState, action) => {
       return {
         ...state,
         posts: payload,
+        loading: false
+      };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map(post =>
+          post._id === payload.postId ? { ...post, likes: payload.likes } : post
+        ),
         loading: false
       };
     case POST_ERROR:
